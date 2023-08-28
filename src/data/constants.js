37c31290606792ecbfa0a754794bbf2f146cc7da
Indrajit Ghosh <indrajitghosh.dev@gmail.com>
2023-08-28 13:35:22 +0530

    1st update

---
+++ src/data/constants.js
@@ -0,0 +1,253 @@
+export const Bio = {
+  name: "Indrajit Ghosh",
+  roles: ["Frontend Developer", "Wordpress Developer", "UI/UX Designer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/devindrajit1998",
+  resume:
+    "https://drive.google.com/file/d/1oelJSsZJWRS_NceIji-yZeyHemhz5BZd/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+  twitter: "https://twitter.com/RishavChanda",
+  insta: "https://www.instagram.com/rishav_chanda/",
+  facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      // {
+      //   name: "Redux",
+      //   image:
+      //     "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      // },
+      // {
+      //   name: "Next Js",
+      //   image:
+      //     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      // },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image: "https://v4.mui.com/static/logo.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png",
+      },
+      // {
+      //   name: "Flutter",
+      //   image:
+      //     "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      // },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      // {
+      //   name: "Docker",
+      //   image:
+      //     "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      // },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      // {
+      //   name: "Postman",
+      //   image:
+      //     "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      // },
+      // {
+      //   name: "Adobe XD",
+      //   image:
+      //     "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      // },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+      {
+        name: "Vercel",
+        image: "https://www.svgrepo.com/show/327408/logo-vercel.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C4D0BAQGrwAmliNVIvQ/company-logo_200_200/0/1603798574284?e=2147483647&v=beta&t=NkgDOLFEu5l9gQW-vMImnXxN8qUsihtZSl6y12v8VSg",
+    role: "Frontend Engineer Intern",
+    company: "Tinta Square",
+    date: "April 2023 - Present",
+    desc: "Working on the frontend of the web application.",
+    skills: ["HTML5", "CSS3", "JavaScript", "Bootstrap", "Wordpress"],
+  },
+  {
+    id: 1,
+    img: "https://career.webindia123.com/career/institutes/aspupload/Uploads/west-bengal/24260/logo.jpg",
+    role: "Digital Marketing Intern",
+    company: "Gargi Memorial Institute Of Technology",
+    date: "January 2023 - April 2023",
+    desc: "Working as Digital Marketing Intern.",
+    skills: ["Creative Design", "Content Writing"],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://career.webindia123.com/career/institutes/aspupload/Uploads/west-bengal/24260/logo.jpg",
+    school: "Gargi Memorial Institute Of Technology",
+    date: "Aug 2020 - Sep 2023",
+    grade: "8.7 up to 7th semester.",
+    desc: "I am currently pursuing a Bachelor's degree in Electrical engineering at Gargi Memorial Institute Of Technology, Kolkata. I have completed 5 semesters and have a CGPA of 8.7",
+    degree: "Bachelor of Technology - BTech, Electrical engineering",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdRdG2mAnQZxpyeBEsdk3tY683EV2OEFjikg&usqp=CAU",
+    school: "Baruipur Government Polytechnic",
+    date: "Aug 2017 - Aug 2020",
+    grade: "77.7%",
+    desc: "I have completed Diploma in Electrical engineering at Baruipur Government Polytechnic, Kolkata. I have completed 6 semesters and have a percentage of 77.7%",
+    degree: "Diploma - Electrical engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "GMIT Website",
+    date: "Apr 2023",
+    description:
+      "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+    image: "https://api.pikwy.com/web/64ec4e3f706cf90ade19b135.jpg",
+    tags: ["HTML", "CSS", "BootStrap", "JavaScript"],
+    category: "html",
+    github: "",
+    webapp: "https://gmit2023.netlify.app",
+    member: [
+      {
+        name: "Indrajit Ghosh",
+        img: "https://avatars.githubusercontent.com/u/131792433?v=4",
+        linkedin: "https://www.linkedin.com/in/indrajit-ghosh-mern-stack/",
+        github: "https://github.com/devindrajit1998",
+      },
+    ],
+  },
+
+  {
+    id: 1,
+    title: "React Calculator",
+    date: "Aug 2023",
+    description:
+      "Create a modern and user-friendly calculator web application using React, showcasing my skills in front-end development and UI design. This project aims to provide a functional calculator tool while demonstrating my proficiency in React components, state management, and responsive design.",
+    image: "https://api.pikwy.com/web/64ec509e36857172270c4a63.jpg",
+    tags: ["React", "JSX", "CSS", "JavaScript ES6+"],
+    category: "react",
+    github: "https://github.com/devindrajit1998/Basic-Calculator---React-Js",
+    webapp: "https://basic-calculator-react-js.vercel.app/",
+    member: [
+      {
+        name: "Indrajit Ghosh",
+        img: "https://avatars.githubusercontent.com/u/131792433?v=4",
+        linkedin: "https://www.linkedin.com/in/indrajit-ghosh-mern-stack/",
+        github: "https://github.com/devindrajit1998",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Global IP convention - GIPC",
+    date: "May 2023",
+    description:
+      "Create a modern and user-friendly calculator web application using React, showcasing my skills in front-end development and UI design. This project aims to provide a functional calculator tool while demonstrating my proficiency in React components, state management, and responsive design.",
+    image: "https://api.pikwy.com/web/64ec51f79e604f7b870bea73.jpg",
+    tags: ["HTML", "CSS", "BootStrap", "JavaScript"],
+    category: "html",
+    github: "",
+    webapp: "https://globalipconvention.com/",
+    member: [
+      {
+        name: "Indrajit Ghosh",
+        img: "https://avatars.githubusercontent.com/u/131792433?v=4",
+        linkedin: "https://www.linkedin.com/in/indrajit-ghosh-mern-stack/",
+        github: "https://github.com/devindrajit1998",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "BarLiq Dashboard",
+    date: "July 2023",
+    description:
+      "Create a modern and user-friendly calculator web application using React, showcasing my skills in front-end development and UI design. This project aims to provide a functional calculator tool while demonstrating my proficiency in React components, state management, and responsive design.",
+    image: "https://api.pikwy.com/web/64ec534b36857172270c4a65.jpg",
+    tags: ["React", "JSX", "CSS", "JavaScript ES6+"],
+    category: "react",
+    github: "!#",
+    webapp: "https://globalipconvention.com/",
+    member: [
+      {
+        name: "Indrajit Ghosh",
+        img: "https://avatars.githubusercontent.com/u/131792433?v=4",
+        linkedin: "https://www.linkedin.com/in/indrajit-ghosh-mern-stack/",
+        github: "https://github.com/devindrajit1998",
+      },
+    ],
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
